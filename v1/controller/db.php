7af8e48ab74ef2e5ba4a0bf3cae2f06d6cad7b57
Paul Evans <pkbevans@mail.com>
2022-12-15 15:53:42 +0000

    Moved DB password out of code

--- v1/controller/db.php
+++ v1/controller/db.php
@@ -1,4 +1,5 @@
 <?php
+require_once ($_SERVER['DOCUMENT_ROOT'].'/ppSecure/paypage.config.php');
 
 class DB {
     private static $writeDBConnection;
@@ -6,7 +7,7 @@ class DB {
 
     public static function connectWriteDB(){
         if(self::$writeDBConnection === null){
-            self::$writeDBConnection = new PDO ('mysql:host=localhost;dbname=paypage;utf8', 'root', '<<PASSWORD>>');
+            self::$writeDBConnection = new PDO ('mysql:host=localhost;dbname=paypage;utf8', 'root', DB_PASSWORD);
             self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
@@ -14,7 +15,7 @@ class DB {
     }
     public static function connectReadDB(){
         if(self::$readDBConnection === null){
-            self::$readDBConnection = new PDO ('mysql:host=localhost;dbname=paypage;utf8', 'root', '<<PASSWORD>>');
+            self::$readDBConnection = new PDO ('mysql:host=localhost;dbname=paypage;utf8', 'root', DB_PASSWORD);
             self::$readDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             self::$readDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         }
