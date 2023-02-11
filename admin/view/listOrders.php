22c2439dab2b0366c98a9fa0ec6657532305f9f7
Paul Evans <pkbevans@mail.com>
2023-02-11 15:16:59 +0000

    completed prev update

--- admin/view/listOrders.php
+++ admin/view/listOrders.php
@@ -7,7 +7,8 @@
             <button type="button" class="btn btn-primary" onclick="refresh()">Refresh</button>
         </div>
     </div>
-    <table class="table table-hover">
+    <div class="row">
+    <table class="table table-hover table-fixed">
         <thead><tr>
             <th>Timestamp</th>
             <th>Order Id</th>
@@ -35,17 +36,21 @@
 <?php endforeach; ?>
         </tbody>
     </table>
-    <nav aria-label="Page navigation example">
+    </div>
+    <nav aria-label="Page navigation">
     <ul class="pagination">
-        <li class="page-item <?php echo ($hasPrev ? '' : ' disabled');?>"><a class="page-link" <?php echo ($hasPrev ? 'onclick="getOrders('.($currentPage-1).')"' : 'tabindex="-1" aria-disabled="true"');?>>Previous</a></li>
+        <li class="page-item <?php echo ($hasPrev ? '' : ' disabled');?>"><a class="page-link" <?php echo ($hasPrev ? 'onclick="getOrders('.(1).')"' : 'tabindex="-1" aria-disabled="true"');?>><<</a></li>
+        <li class="page-item <?php echo ($hasPrev ? '' : ' disabled');?>"><a class="page-link" <?php echo ($hasPrev ? 'onclick="getOrders('.($currentPage-1).')"' : 'tabindex="-1" aria-disabled="true"');?>><</a></li>
 <?php
-$x = 1;
-while($x < ($currentPage+15) && $x <= $totalPages){
+$start = ($currentPage-10)<1?1:$currentPage-10;
+$x = $start;
+while($x < ($start+15) && $x <= $totalPages){
     echo '<li class="page-item'. ($x==$currentPage?' active':'').'"><a class="page-link" onclick="getOrders('.$x.')">'.$x.'</a></li>';
     ++$x;
 }
 ?>
-        <li class="page-item <?php echo ($hasNext ? '' : ' disabled');?>"><a class="page-link" <?php echo ($hasNext ? 'onclick="getOrders('.($currentPage+1).')"' : 'tabindex="-1" aria-disabled="true"');?>>Next</a></li>
+        <li class="page-item <?php echo ($hasNext ? '' : ' disabled');?>"><a class="page-link" <?php echo ($hasNext ? 'onclick="getOrders('.($currentPage+1).')"' : 'tabindex="-1" aria-disabled="true"');?>>></a></li>
+        <li class="page-item <?php echo ($hasNext ? '' : ' disabled');?>"><a class="page-link" <?php echo ($hasNext ? 'onclick="getOrders('.($totalPages).')"' : 'tabindex="-1" aria-disabled="true"');?>>>></a></li>
     </ul>
     </nav>
 <?php if (!$orders): ?>
