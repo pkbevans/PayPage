8d3d579454cf845b0d9f8d6ca5f01ccf412318b6
Paul Evans <pkbevans@mail.com>
2023-02-14 14:38:10 +0000

    Device fingerprinting fix

--- checkout/api/checkout.php
+++ checkout/api/checkout.php
@@ -32,7 +32,7 @@ if(isset($_REQUEST['orderHash']) && isset($_REQUEST['orderId'])){
 <html lang="en-GB">
 <head>
     <!-- Device Fingrprinting -->
-    <script type="text/javascript" src="https://h.online-metrix.net/fp/tags.js?org_id=<?php echo $TMOrgId?>&session_id=<?php echo CHILD_MID?><?php echo $sessionId?>"></script>
+    <script type="text/javascript" src="https://h.online-metrix.net/fp/tags.js?org_id=<?php echo $TMOrgId?>&session_id=<?php echo empty(CHILD_MID)?MID:CHILD_MID?><?php echo $sessionId?>"></script>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -44,7 +44,7 @@ if(isset($_REQUEST['orderHash']) && isset($_REQUEST['orderId'])){
 </head>
 <body>
     <!-- Threatmetrix device fingerprinting -->
-    <iframe style="width: 100px;height: 100px;border: 0;position: absolute;top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=<?php echo $TMOrgId?>&session_id=<?php echo CHILD_MID?><?php echo $sessionId?>"></iframe>
+    <iframe style="width: 100px;height: 100px;border: 0;position: absolute;top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=<?php echo $TMOrgId?>&session_id=<?php echo empty(CHILD_MID)?MID:CHILD_MID?><?php echo $sessionId?>"></iframe>
     <!--Cardinal device data collection code START-->
     <iframe id="cardinal_collection_iframe" name="collectionIframe" height="1" width="1" style="display: none;"></iframe>
     <form id="cardinal_collection_form" method="POST" target="collectionIframe" action="">
