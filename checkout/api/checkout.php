3404e7d816cd956da41ba056d0a72bc374867aad
Paul Evans <pkbevans@mail.com>
2023-03-10 19:49:13 +0000

    Device fingerprinting correction

--- checkout/api/checkout.php
+++ checkout/api/checkout.php
@@ -44,7 +44,10 @@ if(isset($_REQUEST['orderHash']) && isset($_REQUEST['orderId'])){
 </head>
 <body>
     <!-- Threatmetrix device fingerprinting -->
-    <iframe style="width: 100px;height: 100px;border: 0;position: absolute;top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=<?php echo $TMOrgId?>&session_id=<?php echo empty(CHILD_MID)?MID:CHILD_MID?><?php echo $sessionId?>"></iframe>
+    <noscript>
+        <!-- If javascript disabled...-->
+        <iframe style="width: 100px;height: 100px;border: 0;position: absolute;top: -5000px;" src="https://h.online-metrix.net/fp/tags?org_id=<?php echo $TMOrgId?>&session_id=<?php echo empty(CHILD_MID)?MID:CHILD_MID?><?php echo $sessionId?>"></iframe>
+    </noscript>
     <!--Cardinal device data collection code START-->
     <iframe id="cardinal_collection_iframe" name="collectionIframe" height="1" width="1" style="display: none;"></iframe>
     <form id="cardinal_collection_form" method="POST" target="collectionIframe" action="">
